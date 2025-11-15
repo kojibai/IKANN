b92559e4767a7a7ca075ee7ecf770bfcda93f2c5
Kojib <kojibchat@gmail.com>
2025-11-15 15:40:35 -0500

    v1.2

--- start.sh
+++ start.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-
-# Render uses Linux container paths; set working dir
 cd "$(dirname "$0")"
-
-# Optional: Handle GOMAXPROCS warning
 export GOMAXPROCS=1
-
-# Run CoreDNS using relative paths
 exec ./coredns -conf Corefile
